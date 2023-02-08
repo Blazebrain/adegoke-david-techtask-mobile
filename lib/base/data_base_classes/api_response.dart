a6b88bf7bec6d680ee1d9707b0aee15809c3e79f
Blazebrain <davidadegoke16@gmail.com>
2023-02-08 08:04:38 +0100

    chore: Folder and project dependencies setup

---
+++ lib/base/data_base_classes/api_response.dart
@@ -0,0 +1,44 @@
+import 'failures.dart';
+import 'dart:core';
+
+class ApiResponse<T> {
+  final T? data;
+  final Failure? error;
+  final bool hasError;
+
+  ApiResponse({
+    this.data,
+    this.error,
+  })  : hasError = error != null,
+        assert((data != null) || (error != null),
+            'Must have one of data or error');
+}
+
+mixin Converter<E> on ApiResponse<Map<String, dynamic>> {
+  ApiResponse<E> transform<E>(
+    E Function(Map<String, dynamic> data) transformer, {
+    bool ignoreError = false,
+  }) {
+    E? transformedData;
+    if (data != null)
+      transformedData = (ignoreError || !hasError) ? transformer(data!) : null;
+    return ApiResponse<E>(
+      data: transformedData,
+      error: error,
+    );
+  }
+
+  ApiResponse<bool> transformToStatusOnly() {
+    bool status = false;
+    if (data != null) status = data!['status'] == 'success';
+
+    return ApiResponse(
+      data: status,
+      error: error,
+    );
+    // return transform((data) {
+    //   final status = data['success'] as bool;
+    //   return status ?? false;
+    // });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-          ],
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
       ),
     );
   }
